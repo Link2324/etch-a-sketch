c6afc5177a86307f4d83712ddc7e89c72b479387
Mangal-Kushwah <63869813+Link2324@users.noreply.github.com>
2024-02-19 12:25:41 +0530

    Add event listerners so grid element will hightlight in blue on hover

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ function createGrid(size = 16) {
 
     if (size < 1 || size > 100) {
         alert("Size should be in range of [1, 100]");
-        return;        
+        return;
     }
 
     const girdElementSize = canvas.clientHeight / size;
@@ -19,7 +19,8 @@ function createGrid(size = 16) {
             const div = document.createElement('div');
             div.style.cssText = `border: 1px solid lightgrey; 
                                  width: ${girdElementSize}px; 
-                                 height: ${girdElementSize}px; flex: 0 0 1;`;
+                                 height: ${girdElementSize}px; flex: 0 0 1;
+                                 `;
             row.appendChild(div);
         }
         canvas.appendChild(row);
@@ -27,3 +28,13 @@ function createGrid(size = 16) {
 }
 
 createGrid(16);
+
+canvas.addEventListener('mouseover', e => {
+    const target = e.target;
+    target.style['background-color'] = 'lightblue';
+})
+
+canvas.addEventListener('mouseout', e => {
+    const target = e.target;
+    target.style['background-color'] = 'white';
+})
